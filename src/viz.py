1f5cb4b7d86e49a04598fec46bac742bde52fe73
Shara Hysén <shara.hysen@outlook.com>
2025-10-23 16:34:09 +0200

    Skapat förslag på struktur för att få en öveblick

---
+++ src/viz.py
@@ -0,0 +1,11 @@
+"""läs in funktioner från metrics"""
+
+
+"""Skapa olika diagram med värden från metrics
+
+1. Vad säljer vi - vilka kategorier driver mest intäkt
+                 - AVO per kategori 
+2. Var säljer vi - vilka städer driver mest intäkt
+                 - AVO per stad
+3. När säljer vi - intäkt per månad?
+4. Top 3         - vilka tre kategorier driver mest intäkt"""
