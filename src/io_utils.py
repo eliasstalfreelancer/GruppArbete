954d4991083f88170be8d394a72c46e1964d22ac
Shara Hysén <shara.hysen@outlook.com>
2025-10-23 12:51:33 +0200

    skapat en struktur och förklaringar vad som ska göras

---
+++ src/io_utils.py
@@ -0,0 +1,14 @@
+""" 
+1. Börja med att importera bibliotek pandas
+
+2. Skapa en funktion för att läsa in fil som vi sedan kan använda i Notebooken.
+    Funktionen ska: 
+    *läsa in filen men filepath som vi skickar med
+    *retunera pd.read_csv
+    
+3. Skapa en funktion som 'städar' filen
+    Funktionen ska:
+    *Sätta rätt datatyper så vi kan använda kategorierna för olika beräkningar senare
+    *Retunera df_clean"""
+
+
