a0b1ed1257aa3bce0d1e856d5068fea2f86ca4fb
Elias <eliasstalfreelancer@gmail.com>
2025-10-27 10:26:51 +0100

    Adjusted the struckture of ecommerce and metrics

--- src/ecommerce.py
+++ src/ecommerce.py
@@ -1,60 +1,34 @@
 """Läs in funktioner från metrics"""
 
 
-import io_utils
+
 import pandas as pd
+#används för att få ut summan av enheter och intäkt
 
-class DataProccesing:
-    def __init__(self,path):
-        data = io_utils.load_data(path)
-        clean_data = io_utils.clean_data(data)
-        clean_data_df = pd.DataFrame(clean_data)
-        
-        amount_of_orders = clean_data_df.groupby("city",observed=False)["revenue"].count()
-        amount_of_orders_df = pd.DataFrame(amount_of_orders)
-        amount_of_orders_df = amount_of_orders_df.rename(columns={"revenue": "Amount of orders"})
+def amount_of_orders(df):
+    
+    amount_of_orders = df.groupby("city",observed=False)["revenue"].count()
+    amount_of_orders_df = pd.DataFrame(amount_of_orders)
+    amount_of_orders_df = amount_of_orders_df.rename(columns={"revenue": "Amount of orders"})
+    return amount_of_orders_df
 
-        #exmpel på hur man kallar fram en variable som liger i self:
-        #DataProccesing("data/ecommerce_sales.csv").amount_of_orders_df
-        #Output: 2500
-        self.amount_of_orders_df = amount_of_orders_df
-        self.clean_data_df = clean_data_df
+def totel_index_from_difrent_cuntries(df,index):
+    sum = df.groupby("city").agg(index_sum = (index,"sum")).sort_values("index_sum",ascending=False)
         
+    return sum
 
-    #används för att få ut summan av enheter och intäkt
-    def totel_index_from_difrent_cuntries(self,index):
-        clean_data_df = self.clean_data_df
-        sum = clean_data_df.groupby("city").agg(index_sum = (index,"sum")).sort_values("index_sum",ascending=False)
+def average_vaule_order(df):
         
-        return sum
+    avo = df.groupby("city",observed=False)["revenue"].mean()
+    avo_df = pd.DataFrame(avo)
+    avo_df = avo_df.round(2)
+    avo_df = avo_df.rename(columns={"revenue": "AVO"})
+        
+        
+    return avo_df
 
-    def avo(self):
-        
-        clean_data_df = self.clean_data_df
-        
-        avo = clean_data_df.groupby("city",observed=False)["revenue"].mean()
-        avo_df = pd.DataFrame(avo)
-        avo_df = avo_df.round(2)
-        avo_df = avo_df.rename(columns={"revenue": "AVO"})
-        
-        
-        return avo_df
     
 
-
-    
-def key_words():
-    avo = DataProccesing("data/ecommerce_sales.csv").avo()
-    revenue = DataProccesing("data/ecommerce_sales.csv").totel_index_from_difrent_cuntries("revenue")
-    unints = DataProccesing("data/ecommerce_sales.csv").totel_index_from_difrent_cuntries("units")
-    orders = DataProccesing("data/ecommerce_sales.csv").amount_of_orders_df
-    
-    
-    result = orders
-    result["unints"] = unints
-    result["revenue"] = revenue
-    result["avo"] = avo
-    return result
 
 
 
--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,33 @@
 
 """Här samlar vi alla utsräkningar vi behöver till vår klass i ecommerce.py som presenterar nyckeltal"""
+import io_utils
+import pandas as pd
+import ecommerce
 
+class DataProccesing:
+    def __init__(self,path):
+        data = io_utils.load_data(path)
+        clean_data = io_utils.clean_data(data)
+        clean_data_df = pd.DataFrame(clean_data)
+        #exmpel på hur man kallar fram en variable som liger i self:
+        #DataProccesing("data/ecommerce_sales.csv").amount_of_orders_df
+        self.clean_data_df = clean_data_df
+        
+    def key_words(self):
+        avo = ecommerce.average_vaule_order(df=self.clean_data_df)
+        revenue = ecommerce.totel_index_from_difrent_cuntries(df=self.clean_data_df,index="revenue")
+        unints =  ecommerce.totel_index_from_difrent_cuntries(df=self.clean_data_df,index="units")
+        orders = ecommerce.amount_of_orders(df=self.clean_data_df)
+    
+        
+        result = orders
+        result["unints"] = unints
+        result["revenue"] = revenue
+        result["avo"] = avo
+        return result
+
+
+print(DataProccesing("data/ecommerce_sales.csv").key_words())
 
 """Här vill vi smala alla uträkningar som vi ska använda för att sedan skapa diagram i viz.py"""
 
