59a5b38219eb90de54f7d0a2ac7f10fd949ca99a
Shara-Hysen <shara.hysen@outlook.com>
2025-10-27 09:09:53 +0100

    Update io_utils.py

--- src/io_utils.py
+++ src/io_utils.py
@@ -19,7 +19,7 @@ def load_data(filepath):
 
 def clean_data(df):
     df_clean = df
-    df_clean["date"] = pd.to_datetime(df_clean["date"], dayfirst=True, errors="coerce")
+    df_clean["date"] = pd.to_datetime(df_clean["date"], errors="coerce")
     df_clean["city"] = df_clean["city"].astype("category")
     df_clean["category"] = df_clean["category"].astype("category")
     return df_clean
