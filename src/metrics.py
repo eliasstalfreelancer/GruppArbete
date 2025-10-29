5661c8bf5bbcb85e2ab0d3289f52c58dae549f11
polecat69-jl <info@jldigital.se>
2025-10-29 12:49:20 +0100

    Fixade city_revenue

--- src/metrics.py
+++ src/metrics.py
@@ -4,3 +4,20 @@
 
 """Här vill vi smala alla uträkningar som vi ska använda för att sedan skapa diagram i viz.py"""
 
+
+
+import pandas as pd
+from io_utils import load_data, clean_data
+
+df = load_data("data/ecommerce_sales.csv")
+df_clean = clean_data(df)
+
+def city_revenue(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df.groupby("city")["revenue"]
+        .sum()
+        .sort_values(ascending=False)
+    )
+
+city_revenue_result = city_revenue(df_clean)
+print(city_revenue_result)
