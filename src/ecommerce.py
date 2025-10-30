3e5e849ab663f43b63ee7ab958395db553deb8b0
Elias <eliasstalfreelancer@gmail.com>
2025-10-30 15:11:56 +0100

    changed some call names

--- src/ecommerce.py
+++ src/ecommerce.py
@@ -1,9 +1,9 @@
 """Läs in funktioner från metrics"""
 
 
-import src.io_utils as io_utils
+
 import pandas as pd
-import src.metrics as metrics
+import src.metrics as me
 
 class DataProccesing:
     def __init__(self,df):
@@ -15,10 +15,10 @@ class DataProccesing:
         self.data_df = data_df
         
     def key_words(self):
-        avo = metrics.average_vaule_order(df=self.data_df)
-        revenue = metrics.total_index(df=self.data_df,index="revenue")
-        unints =  metrics.total_index(df=self.data_df,index="units")
-        orders = metrics.amount_of_orders(df=self.data_df)
+        avo = me.average_vaule_order(df=self.data_df)
+        revenue = me.total_index(df=self.data_df,index="revenue")
+        unints =  me.total_index(df=self.data_df,index="units")
+        orders = me.amount_of_orders(df=self.data_df)
     
         
         
@@ -38,7 +38,7 @@ class DataProccesing:
 
 
 
-"""Skapar vi en klass som hämtar värden från metrics.
+"""Skapar vi en klass som hämtar värden från me.
 Klassen ska retunera våra nyckelvärden, förslagvis:
 - Antal ordrar
 - Total antal enheter
